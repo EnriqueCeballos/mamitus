234bcfeacac8f1f190d4c7d728f68d86eaec6a32
Enrique Ceballos <enrique_cpl12@hotmail.com>
2022-01-27 20:49:02 -0300

    Nombres en Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
         <a className="navbar-brand" href="#">
-          Navbar
+          MAMITUS
         </a>
         <button
           className="navbar-toggler"
@@ -22,12 +22,12 @@ const Navbar = () => {
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <a className="nav-link active" aria-current="page" href="#">
-                Home
+                Inicio
               </a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#">
-                Link
+                Ferias
               </a>
             </li>
             <li className="nav-item dropdown">
@@ -39,17 +39,17 @@ const Navbar = () => {
                 data-bs-toggle="dropdown"
                 aria-expanded="false"
               >
-                Dropdown
+                Productos
               </a>
               <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
                 <li>
                   <a className="dropdown-item" href="#">
-                    Action
+                    Alfombras
                   </a>
                 </li>
                 <li>
                   <a className="dropdown-item" href="#">
-                    Another action
+                    Canastos
                   </a>
                 </li>
                 <li>
@@ -57,24 +57,24 @@ const Navbar = () => {
                 </li>
                 <li>
                   <a className="dropdown-item" href="#">
-                    Something else here
+                    Macetas
                   </a>
                 </li>
               </ul>
             </li>
             <li className="nav-item">
-              <a className="nav-link disabled">Disabled</a>
+              <a className="nav-link disabled">Nosotros</a>
             </li>
           </ul>
           <form className="d-flex">
             <input
               className="form-control me-2"
               type="search"
-              placeholder="Search"
+              placeholder="Buscar"
               aria-label="Search"
             />
             <button className="btn btn-outline-success" type="submit">
-              Search
+              Buscar
             </button>
           </form>
         </div>
